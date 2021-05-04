84cedd359e3361d573f69e1d78427c3d4574b8de
MJP8 <malachi@timelapseplus.com>
2021-05-04 17:49:30 -0500

    fixed #1 -- changed strcmp() to strncmp()

--- markdown.c
+++ markdown.c
@@ -1,31 +1,29 @@
-#include <stdio.h>
-#include <string.h>
-int main(int argc, char *argv[]) {
-	char read_file[300];
-	char types[10][20] = {"# ", "## ", ""};
-	char converted_types[10][20] = {"<h1>", "<h2>"};
-	if (argc > 2) {
-		FILE *file;
-		int value;
-		file = fopen(argv[1], "rb");
-		if (file) {
-			while (1) {
-				value = fgetc(file);
-				if (value == EOF) break;
-				else sprintf(read_file, "%s%c", read_file, value);
+#include <stdio.h> // include a standard I/O library
+#include <string.h> // include a library for manipulating strings
+int main(int argc, char *argv[]) { // define the function that automatically runs
+	char read_file[300]; // make a string of 300 characters or less named "read_file"
+	char types[10][20] = {"# ", "## ", ""}; // make a array of strings named "types"
+	char converted_types[10][20] = {"<h1>", "<h2>"}; // make another array of strings named "converted_types"
+	if (argc > 2) { // if there are two or more parameters then...
+		FILE *file; // create a pointer to a file
+		int value; // create a integer varible
+		file = fopen(argv[1], "rb"); // open the file the user specifies
+		if (file) { // if the file was opened successfully then...
+			while (1) { // repeat this forever (unless you type "break")
+				value = fgetc(file); // set the value varible to a character in the file
+				if (value == EOF) break; // if it is the end of the file exit the loop
+				else sprintf(read_file, "%s%c", read_file, value); // otherwise add the character to the varible "read_file"
 			}
-			fclose(file);
+			fclose(file); // close the file
 		} else {
 			printf("Error: incorrect path to file\n");
-			printf("%s", argv[1]);
 		}
 		if (strlen(read_file) != 0) {
 			char *type = types[0];
 			int i = 0;
 			do {
-				printf("checking string");
-				if (strcmp(type, strstr(read_file, type))) {
-					printf("Found a '%s'!", type);
+				if (!strncmp(type, read_file, strlen(type))) {
+					printf("Found a '%s'!\n", type);
 				}
 				i++;
 				type = types[i];
